ca189d2be6f878bed70ae0eca735f5c90a3da4c7
Mustafa Adam <mustafajeja@gmail.com>
2022-04-20 21:30:24 +0200

    Add error message and sumbit on keydown:Enter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
 	let todos = [];
-
 	let task = '';
+	let error = '';
 
 	const addTask = () => {
 		let newTask = {
@@ -9,8 +9,12 @@
 			isComplete: false,
 			createdAt: new Date()
 		};
-		todos = [...todos, newTask];
-
+		if (task !== '') {
+			todos = [...todos, newTask];
+			error = '';
+		} else {
+			error = 'You have not typed anything';
+		}
 		task = '';
 	};
 
@@ -20,6 +24,12 @@
 	const removeTask = (index) => {
 		let deleteItem = todos[index];
 		todos = todos.filter((item) => item != deleteItem);
+	};
+
+	const keyIsPressed = (event) => {
+		if (event.code === 'Enter') {
+			addTask();
+		}
 	};
 </script>
 
@@ -44,41 +54,41 @@
 						: ''}"
 				>
 					<span>{todo.task}</span>
-					<span>
-						{#if todo.isComplete}
-							<button on:click={() => removeTask(index)}>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="h-6 w-6 text-red-500"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									stroke-width="2"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-									/>
-								</svg>
-							</button>
-						{:else}
-							<button on:click={() => markTaskAsComplete(index)}>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="w-6 h-6 text-green-600"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									stroke-width="2"
-								>
-									<path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-								</svg>
-							</button>
-						{/if}
+					<span class="flex items-center gap-2">
+						<button on:click={() => markTaskAsComplete(index)}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="w-6 h-6 text-green-600"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								stroke-width="2"
+							>
+								<path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+							</svg>
+						</button>
+						<button on:click={() => removeTask(index)}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-6 w-6 text-red-500"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								stroke-width="2"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+								/>
+							</svg>
+						</button>
 					</span>
 				</li>
 			{/each}
+			<p class="text-red-600">{error}</p>
 		</ol>
 	</div>
 </div>
+
+<svelte:window on:keydown={keyIsPressed} />
