061dd35efb88fe126d49c9baa537782dadc5c051
Mustafa Adam <mustafajeja@gmail.com>
2022-04-21 21:25:53 +0200

    Create firebase config

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: 'AIzaSyB2BTxXGjiHFPqlgJNgIKEFRDbOsnSUvhc',
+	authDomain: 'todos-f6dbe.firebaseapp.com',
+	projectId: 'todos-f6dbe',
+	storageBucket: 'todos-f6dbe.appspot.com',
+	messagingSenderId: '664139375721',
+	appId: '1:664139375721:web:1061f8a2d81f48757f2ef8'
+};
